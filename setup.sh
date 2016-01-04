c254b24645729808fd300711b5473bb47307ecec
Shakeel Mohamed <smohamed@splunk.com>
2016-01-04 01:49:58 -0800

    Add setup.sh - just TODOs for now

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# TODO: fill in everything from installing homebrew, etc.
+
+
+# TODO: Install stuff from cask, ie: brew cask install ksdiff
+
+# TODO: install Sublime Text
+
+# TODO: specific stow commands for sublime, etc.
+
+### STOW ###
+
+# stow bash
+# stow zsh
+# stow -t ~/Library/Application\ Support/Sublime\ Text\ 3/ sublime 
