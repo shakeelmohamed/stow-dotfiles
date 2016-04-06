a9b9dffa540f3fd4b643df0f4710cbb9b3664a4f
Shakeel <shakeel@shakeelmohamed.com>
2016-04-05 17:16:28 -0700

    Add start of a setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 # TODO: fill in everything from installing homebrew, etc.
 
-
 # TODO: Install stuff from cask, ie: brew cask install ksdiff
 
-# TODO: install Sublime Text
-
-# TODO: specific stow commands for sublime, etc.
-
 ### STOW ###
-
-# stow bash -t ~
-# stow zsh -t ~
-# stow sublime -t ~/Library/Application\ Support/Sublime\ Text\ 3/
+stow bash -t ~
+rm ~/.zshrc
+stow zsh -t ~
+stow sublime -t ~/Library/Application\ Support/Sublime\ Text\ 3/
