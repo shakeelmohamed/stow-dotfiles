e55038eac86d6359b6114198e58077d4a278c6d8
Shakeel Mohamed <smohamed@splunk.com>
2016-05-09 21:38:27 -0700

    Make ssh connections stay alive a bit longer
    http://superuser.com/questions/98562/way-to-avoid-ssh-connection-timeout-freezing-of-terminal-tab

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 # Basic file system setup
-cd ~/ && mkdir work && mkdir work/git
+cd $HOME/ && mkdir work && mkdir work/git
 
 # TODO: fill in everything from installing homebrew, etc.
 
 # TODO: Install stuff from cask, ie: brew cask install ksdiff
 
 ### STOW ###
-stow bash -t ~
-rm ~/.zshrc
-stow zsh -t ~
-stow sublime -t ~/Library/Application\ Support/Sublime\ Text\ 3/
+stow bash -t $HOME
+rm $HOME/.zshrc
+stow zsh -t $HOME
+stow sublime -t $HOME/Library/Application\ Support/Sublime\ Text\ 3/
+stow ssh -t $HOME/
