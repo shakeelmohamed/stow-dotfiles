3ab4451acd18ec87a7d5bfd8d89351ff120d39f3
Shakeel Mohamed <contact@shakeel.xyz>
2016-06-21 22:56:28 -0700

    #quoteitup 🔥

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ cd $HOME/ && mkdir work && mkdir work/git
 stow bash -t $HOME
 rm $HOME/.zshrc
 stow zsh -t $HOME
-stow sublime -t $HOME/Library/Application\ Support/Sublime\ Text\ 3/
+stow sublime -t "$HOME/Library/Application\ Support/Sublime\ Text\ 3/"
 stow ssh -t $HOME/
